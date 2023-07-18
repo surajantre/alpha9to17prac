6d38db4e706eee7ccb9be9596651b7fb80195f72
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-07-18 18:03:19 -0500

    first commit

---
+++ oops.java
@@ -0,0 +1,93 @@
+// class pen{
+//     String colour;
+//     String type;
+
+//     public void printColour(){
+//         System.out.println(this.colour);
+
+//     }
+// }
+// public class oops{
+//     public static void main(String[] args){
+//         pen pen1 = new pen();
+//         pen1.colour="blue";
+//         pen1.type="ball";
+
+//         pen pen2 = new pen();
+//         pen2.colour="white";
+//         pen2.type="gel";
+        
+//         pen1.printColour();
+//         pen2.printColour();
+// }
+// }
+
+
+
+
+
+//  class student{
+//     String name;
+//     String description;
+//     int age;
+
+//     public void printInfo(){
+//         System.out.println(this.name);
+//         System.out.println(this.description);
+//         System.out.println(this.age);
+//     }
+//  }
+
+//  public class oops{
+//     public static void main(String[] args){
+//         student s1 = new student();
+//         s1.name = "suraj antre";
+//         s1.description="12 lakh packege in college";
+//         s1.age = 23;
+        
+
+//         student s2 = new student();
+//         s2.name = "dhiraj antre";
+//         s2.description="12 lakh packege in college";
+//         s2.age = 20;
+
+//         s1.printInfo();
+//         s2.printInfo();
+//     }
+//  }
+
+
+
+
+
+// class pen{
+//     String colour;
+//     String type;
+
+//     public void printInfo(){
+//         System.out.println(this.colour);
+//         System.out.println(this.typev);
+//     }
+// }
+// public class oops{
+//     public static void main(String[] args){
+//         pen pen1= new pen();
+//         pen1.colour="blue";
+//         pen1.type="ball";
+
+//         pen pen2 = new pen();
+//         pen2.colour = "white";
+//         pen2.type="gel";
+
+
+//         pen1.printInfo();
+//         pen2.printInfo();
+//     }
+// }
+
+
+
+
+
+
+ 
---
+++ scannerclass.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class scannerclass {
+
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+            int n = sc.nextInt();
+
+            for(int i=0; i<=10; i++){
+                System.out.println(i*n);
+
+
+
+        }
+
+    }
+    
+}
