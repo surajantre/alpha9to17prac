b0887599f6a8210143dabc55a128a0b31377dac4
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-07-21 15:10:18 -0500

    practice

---
+++ basicjava.java
@@ -0,0 +1,52 @@
+// import java.util.*;
+// public class basicjava {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int c = sc.nextInt();
+
+//         int sum = a+b+c;
+//         int average = sum/3;
+
+//         System.out.println("average ogf given tree number: " + average);
+
+      
+//     }
+    
+// }
+
+
+
+// import java.util.*;
+// public class basicjava{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+
+//         System.out.println("area of square = "+ a*a);
+//     }
+// }
+
+
+// import java. util.*;
+
+// import javax.sound.sampled.SourceDataLine;
+// public class basicjava{
+
+
+//     public static void main(String []args){
+//         Scanner sc = new Scanner(System.in);
+//         int pen = sc.nextInt();
+//         int pencil = sc.nextInt();
+//         int eraser = sc.nextInt();
+
+//         int gst = pen+pencil+eraser*0.18;
+
+//         int totalbill = pen+pencil+eraser+gst;
+
+//         System.out.println("total bill = "+ totalbill);
+
+//     }
+
+// }
