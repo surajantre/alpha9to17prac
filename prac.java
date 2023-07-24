0bbe82e9cf988df76e98cbd47ba0ed3d2eafd903
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-07-24 15:35:10 -0500

    prac

--- prac.java
+++ prac.java
@@ -1,16 +1,51 @@
+// import java.util.*;
+// public class prac {
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         int pencil = sc.nextInt();
+//         int pen = sc.nextInt();
+//         int eraser = sc.nextInt();
+
+//         int totalBill=(pencil+pen+eraser);
+
+//         float bill = (totalBill+(0.18f*totalBill));
+
+//         System.out.println(bill);
+//     }
+// }
+
+
+
+
 import java.util.*;
-public class prac {
+public class prac{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter n =");
+        int n = sc.nextInt();
+        System.out.println("enter = m");
+        int m = sc.nextInt();
 
-        int pencil = sc.nextInt();
-        int pen = sc.nextInt();
-        int eraser = sc.nextInt();
+        System.out.println("enter operator =9");
+        char opretar = sc.next().charAt(0);
 
-        int totalBill=(pencil+pen+eraser);
+        switch(opretar) {
 
-        float bill = (totalBill+(0.18f*totalBill));
+            case '+': System.out.println(n+m);
+            break;
 
-        System.out.println(bill);
+            case '-': System.out.println(n-m);
+            break;
+
+            case'*': System.out.println(n*m);
+            break;
+
+            case'/':System.out.println(n/m);
+            break;
+
+            default: System.out.println("wrong operator");
+        }
+    
     }
 }
