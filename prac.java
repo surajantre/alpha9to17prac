cd56d850066f94bcca45aa18d5cf5e7a73f7a359
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-07-25 00:45:27 -0500

    prac

--- prac.java
+++ prac.java
@@ -18,34 +18,64 @@
 
 
 
-import java.util.*;
-public class prac{
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter n =");
-        int n = sc.nextInt();
-        System.out.println("enter = m");
-        int m = sc.nextInt();
+// import java.util.*;
+// public class prac{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter n =");
+//         int n = sc.nextInt();
+//         System.out.println("enter = m");
+//         int m = sc.nextInt();
 
-        System.out.println("enter operator =9");
-        char opretar = sc.next().charAt(0);
+//         System.out.println("enter operator =9");
+//         char opretar = sc.next().charAt(0);
 
-        switch(opretar) {
+//         switch(opretar) {
 
-            case '+': System.out.println(n+m);
-            break;
+//             case '+': System.out.println(n+m);
+//             break;
 
-            case '-': System.out.println(n-m);
-            break;
+//             case '-': System.out.println(n-m);
+//             break;
 
-            case'*': System.out.println(n*m);
-            break;
+//             case'*': System.out.println(n*m);
+//             break;
 
-            case'/':System.out.println(n/m);
-            break;
+//             case'/':System.out.println(n/m);
+//             break;
 
-            default: System.out.println("wrong operator");
-        }
+//             default: System.out.println("wrong operator");
+//         }
     
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+
     }
+}
+public class prac{
+    public static void main (String args[]){
+    Student s1 = new Student();
+    s1.name = "Suraj";
+    s1.age = 22;
+
+    s1.printInfo();
+}
 }
