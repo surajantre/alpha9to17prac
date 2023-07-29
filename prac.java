72cc742fd10801083aecd3b0e442d02da6f05fc1
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-07-28 21:21:57 -0500

    c

--- prac.java
+++ prac.java
@@ -132,32 +132,65 @@
 
 
                                                      
- class Student {
+//  class Student {
+//     String name;
+//     int age;
+
+//     public void printInfo(String name){
+//         System.out.println(name);
+//     }
+
+//     public void printInfo(int age){
+//         System.out.println(age);
+
+//     }
+
+//     public void printInfo(String name,int age){
+//         System.out.println(name+" "+age);
+        
+//     }
+// }
+//     public class prac{
+//         public static void main(String[] args){
+//             Student s1 = new Student();
+//             s1.name = "suraj";
+//             s1.age = 21;
+
+//             s1.printInfo(s1.name,s1.age);
+            
+
+//         }
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+class Student{
     String name;
     int age;
 
-    public void printInfo(String name){
-        System.out.println(name);
-    }
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
 
-    public void printInfo(int age){
-        System.out.println(age);
-
-    }
-
-    public void printInfo(String name,int age){
-        System.out.println(name+" "+age);
-        
     }
 }
-    public class prac{
-        public static void main(String[] args){
-            Student s1 = new Student();
-            s1.name = "suraj";
-            s1.age = 21;
+public class prac{
+    public static void main (String args[]){
+    Student s1 = new Student();
+    s1.name = "Suraj";
+    s1.age = 22;
 
-            s1.printInfo(s1.name,s1.age);
-            
+    s1.printInfo();
+}
+}
 
-        }
-    }
+
