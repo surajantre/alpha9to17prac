b4fa41a26792fbde7190763fd1a6c36e95d192e4
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-07-31 18:42:32 -0500

    prac

---
+++ notepadprac/prac.java
@@ -0,0 +1,90 @@
+// import java.util.*;
+
+// public class prac{
+//   public static void main(String []args){
+//      Scanner sc = new Scanner(System.in);
+//      int n = sc. nextInt();
+
+//      int sum =0;
+
+//      int i = 1;
+
+//    while(i<= 10){
+
+//         System.out.print(som+= i);
+
+//         i++;
+// }
+
+
+// }
+// }
+
+
+
+
+
+// public class prac {
+//   public static void main (String [] args){
+//     for(int i = 1; i<= 4 ;i++){
+//       System.out.println("* * * *")  
+// }
+// }
+// }
+
+
+
+
+
+// public class prac {
+//    public static void main(String[] args){
+    
+//     int i = 15092002;
+
+//     while(i>0){
+//       int lastdigit = i%10;
+//       System.out.print(lastdigit+" ");
+//       i = i/10;
+//     }
+//     System.out.println( );
+   
+// }
+// }
+
+
+
+
+
+
+// public class prac{
+//   public static void main(String [] args){
+//       int n = 12345;
+
+//       int  rev = 0;
+    
+//       while(n>0){
+
+//           int lastdigit = n%10;
+//           int re = (rev*10)+lastdigit;
+
+//           System.out.print(re);
+
+//           n=n/10;          
+
+// }
+// System.out.println();
+// }
+// }
+
+
+
+
+
+import java.util.*;
+public class prac{
+  public static void main(String args[]){
+    Scanner sc= new Scanner (System.in);
+
+
+  }
+}
