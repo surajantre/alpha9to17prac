b5766a5fd61b6f475bd1a8b1bbc5c50fc50ef703
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-05 09:52:57 -0500

    pract2

--- alpha9to17prac/pract2.java
+++ alpha9to17prac/pract2.java
@@ -24,30 +24,107 @@
 
 
 
-public class pract2{
-    public static void revercearry(int numbers[]){
-        int first = 0; int last = numbers.length-1;
+// public class pract2{
+//     public static void revercearry(int numbers[]){
+//         int first = 0; int last = numbers.length-1;
 
-        while(first<last){
-            int temp = numbers[first];
-            numbers[first] = numbers[last];
-            numbers[last] = temp;
+//         while(first<last){
+//             int temp = numbers[first];
+//             numbers[first] = numbers[last];
+//             numbers[last] = temp;
 
-            first++;
-            last--;
-
-
-        }
-
-    }
-    public static void main (String[] args) {
-        int numbers[]={1,2,3,5,7,9};
+//             first++;
+//             last--;
 
 
-        for(int i=0; i<numbers.length; i++){
-            System.out.print(numbers[i]+" ");
-        }
-        System.out.println();
-    }
+//         }
 
-}
+//     }
+//     public static void main (String[] args) {
+//         int numbers[]={1,2,3,5,7,9};
+
+
+//         for(int i=0; i<numbers.length; i++){
+//             System.out.print(numbers[i]+" ");
+//         }
+//         System.out.println();
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+// public class pract2{
+//     public static void reversarr(int arr[]){
+//     for (int i=0; i<arr.length; i++){
+
+//         System.out.println(i);
+//     }
+// }
+//     public static void main(String[] args){
+//         int arr []={1,2,6,9,7,3};
+        
+//     }
+// }
+
+
+
+ 
+// public class pract2{
+//     public static int largestvalue(int numbers[]){
+//         int largest=Integer.MIN_VALUE;  
+
+//         for (int i=0;i<numbers.length;i++){
+//             if (largest < numbers[i]){
+//                 largest = numbers[i];
+//             }
+//         }
+//         return largest;
+
+//     }
+     
+    
+//     public static void main(String args[]){
+//         int numbers[]={1,2,3,5,7,9,1};
+     
+
+//     }
+// }
+
+
+
+// public class pract2{
+//     public static void revercearry(int numbers[]){
+//         int first = 0; int last = numbers.length-1;
+
+//         while(first<last){
+//             int temp = numbers[first];
+//             numbers[first] = numbers[last];
+//             numbers[last] = temp;
+
+//             first++;
+//             last--;
+
+
+//         }
+
+//     }
+//     public static void main (String[] args) {
+//         int numbers[]={1,2,3,5,7,9};
+
+
+//         for(int i=0; i<numbers.length; i++){
+//             System.out.print(numbers[i]+" ");
+//         }
+//         System.out.println();
+//     }
+
+// }
+
+
