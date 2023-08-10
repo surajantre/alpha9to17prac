5042dfb3bbba20194e66546e1e2c0ee76d506a1e
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-09 23:02:28 -0500

    pr

--- alpha9to17prac/pract1.java
+++ alpha9to17prac/pract1.java
@@ -378,3 +378,29 @@
 
 //     }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+public class pract{
+    public static void main(String[] args){
+        int n = 7;
+        for(int i =1;i<=n;i++){
+            for (int j =1;j<=n-i+1;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+
