8a214a8ff1e6643c7bdca6e2d9b19b8ebd3459cc
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-11 21:26:26 -0500

    s

---
+++ alpha17to31prac/fri1108.java
@@ -0,0 +1,24 @@
+// public class fri1108{
+//     public static void main (String args[]){
+//         System.out.println("h w");
+//     }
+// }
+
+public class fri1108{
+    public static void Sortings(int arr[]){
+        int start = 0;int end = arr.length-1;
+
+        if(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        }
+
+    }
+    public static void main (String args[]){
+        int arr []={2,5,8,7,4,1};
+        System.out.println(Sortings arr[]);
+
+       
+    }
+}
